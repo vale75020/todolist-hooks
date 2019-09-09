eb2e35b69ceb219e91e82475174dd1dd5c681020
vale75020 <valentinosuma@hotmail.com>
2019-09-09 18:55:37 +0200

    Updates

---
+++ precache-manifest.30295282e3dbb965ba9aec6e4f4bb75c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e7589ce9dabad449ec6d5f0a008e79",
+    "url": "/todolist-hooks/index.html"
+  },
+  {
+    "revision": "e68f04c00d075db713c6",
+    "url": "/todolist-hooks/static/css/main.7e35ef0b.chunk.css"
+  },
+  {
+    "revision": "1eae34a41ed0c7dce4e1",
+    "url": "/todolist-hooks/static/js/2.279c6b2c.chunk.js"
+  },
+  {
+    "revision": "e68f04c00d075db713c6",
+    "url": "/todolist-hooks/static/js/main.30d98083.chunk.js"
+  },
+  {
+    "revision": "53822e1820c7713b306b",
+    "url": "/todolist-hooks/static/js/runtime~main.1c81783f.js"
+  }
+]);
